25aad3727117288dc812e4a550a22c5c8575f7cf
Kelly online <kellyonline2016@gmail.com>
2020-03-09 02:00:16 +0100

    posting to the body using express.json function

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require('express');
 var api = express();
 
-//api.use()
+
+api.use(express.json());
 
 
 
@@ -17,12 +18,21 @@ api.get('/info', (req, res) => {
     message : `Your name is ${req.query.name} and you are a ${req.query.job}`
     })
 
-}).listen(8000);
+})
 
 
 
 api.post('/sendInfo', (req, res) => {
 
     console.log(req.body)
+
+    res.json({
+        name : req.body.name,
+        job : req.body.job,
+    message : `Your name is ${req.body.name} and you are a ${req.body.job}`
+    })
     
 } )
+
+
+api.listen(8000);
